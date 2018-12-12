2ab9cf0044e3511a03e2125f4fbf3099ac462af3
graphicbeacon <jeropp00@gmail.com>
2018-12-12 12:57:48 +0000

    Initial commit. Generates console-full app.

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:hacker_news_scraper/hacker_news_scraper.dart' as hacker_news_scraper;
+
+main(List<String> arguments) {
+  print('Hello world: ${hacker_news_scraper.calculate()}!');
+}
---
+++ test/hacker_news_scraper_test.dart
@@ -0,0 +1,8 @@
+import 'package:hacker_news_scraper/hacker_news_scraper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
